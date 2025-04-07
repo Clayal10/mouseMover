adaa572d6d27a1b1e7f95dd6252966ea51de0456
Clay Shumaker <clayalshu@gmail.com>
2025-04-07 09:51:06 -0700

    Initial setup for project

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # mouseMover
+
+This is a linux kernel module for CS-430 Operating Systems.
+
+## Description
+
+This module acts as a solution to the burdensome issue of using the dreaded mouse :cold_sweat:
+
+When inserting this module, you will be able to control the mouse with the arrow keys and using left click with the pause button.
+
+
+## Inserting
+
+Use the Makefile to build and inserting with the following:
+
+```bash
+make
+sudo insmod mouse.ko
+```
+
+And to clean up:
+
+```bash
+sudo rmmod mouse.ko
+make clean
+```
---
+++ src/mouse.c
@@ -0,0 +1,121 @@
+// Requirements:
+// - Move the mouse with the arrow keys
+//   - Left: 
+//   - Right
+//   - Up: 
+//   - Down: 
+// - Use the pause button as left click
+
+
+#include <linux/module.h>
+//#include <linux/config.h>
+#include <linux/init.h>
+#include <linux/tty.h>          /* For fg_console, MAX_NR_CONSOLES */
+#include <linux/kd.h>           /* For KDSETLED */
+#include <linux/vt.h>
+#include <linux/vt_kern.h>
+#include <linux/console_struct.h>       /* For vc_cons */
+#include <linux/input.h>
+#include <linux/platform_device.h>
+#include <linux/proc_fs.h>
+#include <linux/interrupt.h>
+#include <linux/keyboard.h>
+
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <asm/uaccess.h>
+#include <asm/io.h>
+#include <linux/slab.h>
+#include <linux/ctype.h>
+
+int state = 0;
+struct input_dev *ex3_dev;
+struct timer_list my_timer;
+struct notifier_block nb;
+
+int kb_notifier_fn(struct notifier_block *pnb, unsigned long action, void* data){
+	struct keyboard_notifier_param *kp = (struct keyboard_notifier_param*)data;
+	if(kp->down){
+		
+	}
+
+	return 0;
+}
+
+void my_timer_func(struct timer_list *unused){
+	/* move in a small square */
+	if (state<30) 
+		input_report_rel(ex3_dev, REL_X, 5);
+	else if (state < 60)
+		input_report_rel(ex3_dev, REL_Y, 5);
+	else if (state < 90)
+		input_report_rel(ex3_dev, REL_X, -5);
+	else
+		input_report_rel(ex3_dev, REL_Y, -5);
+
+	if ((state++) >= 120){
+		state = 0;
+	}
+	/*
+	input_report_key(ex3_dev, BTN_LEFT, 1);
+	input_sync(ex3_dev);
+	input_report_key(ex3_dev, BTN_LEFT, 0);
+	*/
+
+	input_sync(ex3_dev);
+
+
+	/* set timer for 0.02 seconds */
+	//mod_timer(&my_timer, jiffies+HZ/125);
+}
+
+
+static int __init ex3_init(void){
+		
+	/* extra safe initialization */
+	ex3_dev = input_allocate_device();
+
+	/* set up descriptive labels */
+	ex3_dev->name = "Mouse Mover";
+	/* phys is unique on a running system */
+	ex3_dev->phys = "A/Fake/Path";
+	ex3_dev->id.bustype = BUS_HOST;
+	ex3_dev->id.vendor = 0x0001;
+	ex3_dev->id.product = 0x0003;
+	ex3_dev->id.version = 0x0100;
+
+	/* this device has two relative axes */
+	set_bit(EV_REL, ex3_dev->evbit);
+	set_bit(REL_X, ex3_dev->relbit);
+	set_bit(REL_Y, ex3_dev->relbit);
+
+	/* it needs a button to look like a mouse */
+	set_bit(EV_KEY, ex3_dev->evbit);
+	set_bit(BTN_LEFT, ex3_dev->keybit);
+
+	/* and finally register with the input core */
+	input_register_device(ex3_dev);
+
+	/* set up a repeating timer */
+	//timer_setup(&my_timer, my_timer_func, 0);
+	//my_timer.expires = jiffies + HZ/10;
+	//add_timer(&my_timer);
+	
+	// Initilize keyboard notifier
+	nb.notifier_call = kb_notifier_fn;
+	register_keyboard_notifier(&nb);
+
+	return 0;
+}
+
+static void __exit ex3_exit(void){
+	del_timer(&my_timer);
+	input_unregister_device(ex3_dev);
+	unregister_keyboard_notifier(&nb);
+}
+
+MODULE_LICENSE("GPL"); 
+module_init(ex3_init);
+module_exit(ex3_exit);
+
